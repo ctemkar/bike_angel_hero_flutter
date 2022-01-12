c43fe6553aeeac6779088f239f233aacd2f2a60d
Chetan Temkar <58204548+ctemkar@users.noreply.github.com>
2022-01-12 18:48:47 +0530

    Fixed Google Maps problem Added web version!!

---
+++ android/app/src/main/kotlin/com/smartshehar/bike_angel_hero_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smartshehar.bike_angel_hero_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyDtkiqTG5FSZp7wbt0iaNCxCUHgNgUzGg8")
+    GMSServices.provideAPIKey("AIzaSyC3wBGAUj3JQSHQLBkqYZsW9FKfP2x17K4")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
